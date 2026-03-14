f6f4af5c157026490db579c8dbfa16c9a8cc06d4
Fi Ali <fidelemanalintsoa@gmail.com>
2026-03-14 18:55:13 +0300

    try to create a navigation in demo

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -2,6 +2,10 @@ const openMenu = document.querySelector("#menu-open-button");
 const closeMenu = document.querySelector("#menu-close-button");
 let startDemo = document.getElementById("startDemo");
 let sourceDemo = document.getElementById("sourceDemo");
+let sourceDemoportfolio = document.getElementById("sourceDemo-portfolio");
+let project = document.getElementById("project");
+let project1 = document.getElementById("project1");
+let btnDemoProject = document.getElementById("btn-demo-project");
 
 openMenu.addEventListener("click", () =>{
     document.body.classList.toggle("open-menu");
@@ -12,6 +16,41 @@ function openDemo(){
     sourceDemo.style.display = "block";
 }
 
+function openProject(){
+    sourceDemoportfolio.style.display = "block";
+    // btnDemoProject.style.cursor = "deseable";
+}
+
+const listImageDemo = document.querySelectorAll('.item-image');
+let currentIndex = 0;
+
+function showItemImage(index){
+    listImageDemo.forEach((t, i)=>{
+        t.classList.toggle('active', i === index);
+    });
+}
+
+document.getElementById('next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % listImageDemo.length;
+    showItemImage(currentIndex);
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+    currentIndex = (currentIndex-1+listImageDemo.length) % listImageDemo.length;
+});
+
+showItemImage(currentIndex);
+
+document.getElementById('next-projectPort').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % listImageDemo.length;
+    showItemImage(currentIndex);
+});
+
+document.getElementById('prev-projectPort').addEventListener('click', () => {
+    currentIndex = (currentIndex-1+listImageDemo.length) % listImageDemo.length;
+});
+
+showItemImage(currentIndex);
 // if (startDemo.onclick()){
 //     sourceDemo.classList.display = "block";
 // }
