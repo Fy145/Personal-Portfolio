2f30c7472e6f4c7799e5583b114cf82fc8166003
= <=>
2026-03-21 19:04:35 +0300

    change style skill and his html

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,11 +1,5 @@
 const openMenu = document.querySelector("#menu-open-button");
 const closeMenu = document.querySelector("#menu-close-button");
-let startDemo = document.getElementById("startDemo");
-let sourceDemo = document.getElementById("sourceDemo");
-let sourceDemoportfolio = document.getElementById("sourceDemo-portfolio");
-let project = document.getElementById("project");
-let project1 = document.getElementById("project1");
-let btnDemoProject = document.getElementById("btn-demo-project");
 
 openMenu.addEventListener("click", () =>{
     document.body.classList.toggle("open-menu");
@@ -34,14 +28,21 @@ window.onscroll = () => {
     });
 };
 
-function openDemo(){
-    sourceDemo.style.display = "block";
-}
+// let startDemo = document.getElementById("startDemo");
+// let sourceDemo = document.getElementById("sourceDemo");
+// let sourceDemoportfolio = document.getElementById("sourceDemo-portfolio");
+// let project = document.getElementById("project");
+// let project1 = document.getElementById("project1");
+// let btnDemoProject = document.getElementById("btn-demo-project");
 
-function openProject(){
-    sourceDemoportfolio.style.display = "block";
+// function openDemo(){
+//     sourceDemo.style.display = "block";
+// }
+
+// function openProject(){
+//     sourceDemoportfolio.style.display = "block";
     // btnDemoProject.style.cursor = "deseable";
-}
+// }
 
 // const listImageDemo = document.querySelectorAll('.demo-imageComm');
 // let currentIndex = 0;
